dea53ad6bad1530007434428a464ffd3229de2d6
Pedro Ruiz <ruizcandaupedro@gmail.com>
2025-06-01 20:08:50 +0200

    arrays 0027(in progress): added tests

--- arrays/0027_remove_elemnt/remove_element_test.go
+++ arrays/0027_remove_elemnt/remove_element_test.go
@@ -1,1 +1,34 @@
 package removeelemnt
+
+import "testing"
+
+
+func TestRemoveElemnt(t *testing.T) {
+	tests := []struct{
+		name string
+		nums  []int
+		val int
+		expectedNums []int
+		
+	}{
+		{"Case 1",[]int{3,2,2,3},3,[]int{2,2}},
+		{"Case 2",[]int{0,1,2,2,3,0,4,2},2,[]int{0,1,3,0,4}},
+	}
+	for _,tc := range tests {
+		t.Run(tc.name,func(t *testing.T) {
+			output := removeElement(tc.nums,tc.val)
+			if length := len(tc.expectedNums); output != length  {
+				t.Errorf("output: %d; expected: %d", output, length)
+			}
+			if !t.Failed() {
+				for i, e := range tc.expectedNums {
+					if e != tc.expectedNums[i] {
+						t.Errorf("array[%d]= %d; expected: %d", i, tc.expectedNums[i], e)
+					}
+				}
+			}
+		})
+		
+	}
+	
+}
