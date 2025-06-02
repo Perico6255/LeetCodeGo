70b3be7c0ed0172ced110e121344df3184015657
Pedro Ruiz <ruizcandaupedro@gmail.com>
2025-06-02 11:00:12 +0200

    arrays/0066: finished

--- arrays/0066_plus_one/plus_one.go
+++ arrays/0066_plus_one/plus_one.go
@@ -2,7 +2,28 @@ package plusone
 
 
 func plusOne(digits []int) []int {
+	l := len(digits)
 
-    
+	for i := l-1; i >0; i-- {
+		digits[i] = digits[i] + 1
+		if digits[i] < 10 {
+			return digits
+		}
+		digits[i]=0
+	}
+
+	if digits[0]==9 {
+		s := make([]int, l+1)
+		s[0] = 1
+		s[1] = 0
+
+		for i := 2; i < l ; i++ {
+			s[i] = digits[i-1]
+		}
+		return s
+		
+	}
+	digits[0]++
 	return digits
+    
 }
--- arrays/0066_plus_one/plus_one_test.go
+++ arrays/0066_plus_one/plus_one_test.go
@@ -16,11 +16,13 @@ func TestPlusOne(t *testing.T) {
 		{"Case 1",[]int{1,2,3},[]int{1,2,4}},
 		{"Case 2",[]int{4,3,2,1},[]int{4,3,2,2}},
 		{"Case 3",[]int{9},[]int{1,0}},
+		{"Case 4",[]int{0},[]int{1}},
+		{"Case 4",[]int{9,9,9},[]int{1,0,0,0}},
 	}
 	for _, tc := range tests {
 		t.Run(tc.name,func(t *testing.T) {
 			output := plusOne(tc.input)
-			if slices.Equal(tc.expected,output) {
+			if !slices.Equal(tc.expected,output) {
 				t.Errorf("Error, expected: %d, output: %d",tc.expected,output)
 			}
 		})
