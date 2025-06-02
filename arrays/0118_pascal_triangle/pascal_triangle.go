84423cab33b08411c676f0503e380ed9456146e4
Pedro Ruiz <ruizcandaupedro@gmail.com>
2025-06-02 21:51:34 +0200

    arrays/0118(finished): solved

--- arrays/0118_pascal_triangle/pascal_triangle.go
+++ arrays/0118_pascal_triangle/pascal_triangle.go
@@ -1,5 +1,20 @@
 package pascaltriangle
 
 func generate(numRows int) [][]int {
+	p := make([][]int,numRows)
+	for i := range numRows {
+		p[i]= make([]int, i + 1)
+		p[i][0] = 1
+		p[i][i] = 1
+	}
+
+	for i, p1 := range p {
+		for j, p2 := range p1 {
+			if p2 == 0 {
+				p1[j] = p[i-1][j-1] + p[i-1][j]
+			}
+		}
+	}
     
+	return p 
 }
--- arrays/0118_pascal_triangle/pascal_triangle_test.go
+++ arrays/0118_pascal_triangle/pascal_triangle_test.go
@@ -1,8 +1,57 @@
 package pascaltriangle
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 
 func TestGenerate(t *testing.T) {
+	tests := []struct{
+		name string
+		input int
+		expected [][]int
+	}{
+		{"Case 1",1, [][]int{
+			{1},
+		}},
+		{"Case 2",2, [][]int{
+			{1},
+			{1,1},
+		}},
+		{"Case 3",3, [][]int{
+			{1},
+			{1,1},
+			{1,2,1},
+		}},
+		{"Case 4",4, [][]int{
+			{1},
+			{1,1},
+			{1,2,1},
+			{1,3,3,1},
+		}},
+		{"Case 5",5, [][]int{
+			{1},
+			{1,1},
+			{1,2,1},
+			{1,3,3,1},
+			{1,4,6,4,1},
+		}},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name,func(t *testing.T) {
+			out := generate(tc.input)
+			for i := range tc.input {
+				if !slices.Equal(out[i],tc.expected[i]) {
+					t.Errorf("line: %d, expected: %d, got: %d",i,tc.expected[i],out[i])
+				}
+				
+			}
+		})
+		
+	}
+
+
 	
 }
