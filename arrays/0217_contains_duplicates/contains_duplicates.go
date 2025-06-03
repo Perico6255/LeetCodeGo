3f006caecfa1398ad96288b6b1c74755f2254b8c
Pedro Ruiz <ruizcandaupedro@gmail.com>
2025-06-03 13:02:30 +0200

    arrays/0217(solverd): finish

--- arrays/0217_contains_duplicates/contains_duplicates.go
+++ arrays/0217_contains_duplicates/contains_duplicates.go
@@ -1,6 +1,13 @@
 package containsduplicates
 
 func containsDuplicate(nums []int) bool {
+	m := map[int]struct{}{}
+	for _, n := range nums {
+		if _,ok:=m[n];ok {
+			return true
+		}
+		m[n] = struct{}{}
+	}
     
 	return false
 }
