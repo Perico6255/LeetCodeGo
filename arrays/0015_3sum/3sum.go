0c5173ed9e7877f3c1490d28afa4bb3208a000bb
Pedro Ruiz <ruizcandaupedro@gmail.com>
2025-06-22 19:41:19 +0200

    arrays/0015(in progress): resolved

--- arrays/0015_3sum/3sum.go
+++ arrays/0015_3sum/3sum.go
@@ -1,37 +1,84 @@
 package sum
 
+import (
+	"slices"
+)
+
 func threeSum(nums []int) [][]int {
-	m := make(map[int]int)
-	r := make(map[int]struct{},len(nums))
-	l := 0
-	for _, n := range nums {
-		if _,ok := m[n]; !ok {
-			nums[l] = n
-			l++
-		}else {
-			r[n] = struct{}{}
-		}
-		m[n]++
+	l := len(nums)
+	if l < 3 {
+		return [][]int{}
 	}
-	sum := make([][]int,0)
-
-
-
-	for vr:= range r {
-
-		l = -2*vr
-		if l == 0 {
-			continue
-		}
-		if  _,ok:= m[l]; ok  {
-			sum = append(sum, []int{vr,vr,l})
+	slices.Sort(nums)
+	t := make([][]int, 0)
+	for i, v := range nums {
+		if v > 0 {
 			break
 		}
-	}
-	if m[0] >2 {
-		sum = append(sum, []int{0,0 , 0})
-	}
+		if i > 0 {
+			if nums[i] == nums[i-1] {
+				continue
+			}
+		}
+		low := i + 1
+		high := l - 1
+		var s int
+		for low < high {
+			s = v + nums[low] + nums[high]
+			switch {
+			case s < 0:
+				low++
+			case s > 0:
+				high--
+			case s == 0:
+				last_low := nums[low]
+				last_high := nums[high]
 
-
-	return sum 
+				t = append(t, []int{v, last_low, last_high})
+				for low < high && nums[low] == last_low {
+					low++
+				}
+				for low < high && nums[high] == last_high {
+					high--
+				}
+			}
+		}
+	}
+	return t
 }
+
+// func threeSum(nums []int) [][]int {
+// 	m := make(map[int]int)
+// 	r := make(map[int]struct{},len(nums))
+// 	l := 0
+// 	for _, n := range nums {
+// 		if _,ok := m[n]; !ok {
+// 			nums[l] = n
+// 			l++
+// 		}else {
+// 			r[n] = struct{}{}
+// 		}
+// 		m[n]++
+// 	}
+// 	sum := make([][]int,0)
+//
+//
+//
+// 	for vr:= range r {
+//
+// 		l = -2*vr
+// 		if l == 0 {
+// 			continue
+// 		}
+// 		if  _,ok:= m[l]; ok  {
+// 			sum = append(sum, []int{vr,vr,l})
+// 			break
+// 		}
+// 	}
+// 	if m[0] >2 {
+// 		sum = append(sum, []int{0,0 , 0})
+// 	}
+//
+//
+// 	return sum
+// }
--- arrays/0015_3sum/3sum_test.go
+++ arrays/0015_3sum/3sum_test.go
@@ -4,30 +4,29 @@ import (
 	"testing"
 )
 
-
 func TestThreeSum(t *testing.T) {
-	tests := []struct{
-		name string
-		nums []int
+	tests := []struct {
+		name     string
+		nums     []int
 		expected [][]int
 	}{
-		{"Case 1",[]int{-1,0,1,2,-1,-4},[][]int{{-1,-1,2},{-1,0,1}}},
-		{"Case 2",[]int{0,1,1},[][]int{}},
-		{"Case 3",[]int{0,0,0},[][]int{{0,0,0}}},
+		{"Case 1", []int{-1, 0, 1, 2, -1, -4}, [][]int{{-1, -1, 2}, {-1, 0, 1}}},
+		{"Case 2", []int{0, 1, 1}, [][]int{}},
+		{"Case 3", []int{0, 0, 0}, [][]int{{0, 0, 0}}},
 	}
-	
+
 	for _, tc := range tests {
-		t.Run(tc.name,func(t *testing.T) {
+		t.Run(tc.name, func(t *testing.T) {
 			got := threeSum(tc.nums)
 			if len(got) != len(tc.expected) {
-				t.Fatalf("Not same lenght \nexpected: %d, got: %d",tc.expected, got)
-			}else {
+				t.Fatalf("Not same lenght \nexpected: %d, got: %d", tc.expected, got)
+			} else {
 				t.Log("Same Lenght!!")
-				t.Logf("expected: %d",tc.expected)
-				t.Logf("got: %d",got)
+				t.Logf("expected: %d", tc.expected)
+				t.Logf("got: %d", got)
 			}
-			
+
 		})
-		
+
 	}
 }
