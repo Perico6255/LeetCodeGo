3eb7d90b1135d3bfada1e698a46d795840edb530
Pedro Ruiz <ruizcandaupedro@gmail.com>
2025-06-24 18:43:58 +0200

    arrays/0046_permutations(solved): finished

--- arrays/0046_permutations/permutations.go
+++ arrays/0046_permutations/permutations.go
@@ -1,1 +1,51 @@
 package permutations
+
+
+func permute(nums []int) [][]int {
+	l := len(nums)
+	// if l == 1 {
+	// 	return [][]int{nums}
+	// }
+	var c = 1
+	for i := 2; i <= l; i++ {
+		c *= i
+	}
+	sol := make([][]int, c)
+	c = 0
+	step(sol,nums,&c,l,l)
+
+
+
+	return sol
+}
+
+func swap(nums []int, l, n1, n2 int) []int {
+	n := make([]int,l)
+	copy(n,nums)
+	n[n1] = nums[n2]
+	n[n2] = nums[n1]
+	return n
+}
+
+
+func step(sol [][]int, n0 []int, s *int, p, l int)  {
+	if p == 2 {
+		sol[*s] = n0
+		*s++
+		sol[*s] = swap(n0, l, l-1, l-2)
+		*s++
+		return
+	}
+	step(sol,n0,s,p-1,l)
+	for i := 1; i < p; i++ {
+		step(sol, 
+			swap(n0,l, l- p, l-i ),
+			s,
+			p - 1,
+			l)
+	}
+}
+
+
+
+
--- arrays/0046_permutations/permutations_test.go
+++ arrays/0046_permutations/permutations_test.go
@@ -1,1 +1,48 @@
 package permutations
+
+import "testing"
+
+
+func TestPermute(t *testing.T) {
+	tests := []struct{
+		name string
+		nums []int
+		expected [][]int
+	}{
+		{"Case 1", []int{1,2,3}, [][]int{{1,2,3},{1,3,2},{2,1,3},{3,1,2},{3,2,1}}},
+		{"Case 2", []int{0,1}, [][]int{{0,1},{1,0}}},
+		{"Case 3", []int{2,3,4,5}, [][]int{{2,3,4,5},{2,3,5,4},{2,4,3,5},{2,4,5,3},{2,5,3,4},{2,5,4,3},{3,2,4,5},{3,2,5,4},{3,4,2,5},{3,4,5,2},{3,5,2,4},{3,5,4,2},{4,2,3,5},{4,2,5,3},{4,3,2,5},{4,3,5,2},{4,5,2,3},{4,5,3,2},{5,2,3,4},{5,2,4,3},{5,3,2,4},{5,3,4,2},{5,4,2,3},{5,4,3,2},},
+	},}
+	for _, tc := range tests {
+		t.Run(tc.name,func(t *testing.T) {
+			got := permute(tc.nums)
+			if len(tc.expected) != len(got) {
+				t.Fatalf("Not even same lenght, expected: %d, got: %d", tc.expected, got)
+			}
+		})
+		
+	}
+
+	
+}
+func TestPermute2(t *testing.T) {
+	tests := []struct{
+		name string
+		nums []int
+		expected [][]int
+	}{
+		{"Case 1", []int{1,2,3}, [][]int{{1,2,3},{1,3,2},{2,1,3},{3,1,2},{3,2,1}}},
+		{"Case 1", []int{1,2,3,4,5}, [][]int{}},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name,func(t *testing.T) {
+			got := permute(tc.nums)
+			if len(tc.expected) != len(got) {
+				t.Fatalf("Not even same lenght, expected: %d, got: %d", tc.expected, got)
+			}
+		})
+		
+	}
+
+	
+}
